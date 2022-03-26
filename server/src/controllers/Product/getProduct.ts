c3d5c29a51885c864778ba2c768a27bb6b90da1b
khangb1910652 <nguyenduykhang444@.com>
2022-03-26 21:10:50 +0700

    products

---
+++ server/src/controllers/Product/getProduct.ts
@@ -0,0 +1,72 @@
+import express from "express";
+import { connection } from "../../database/mysql";
+
+const router = express.Router();
+
+export const getProduct = () => {
+  return router.get(
+    "/",
+    async (req: express.Request, res: express.Response) => {
+      try {
+        let data = {
+          idHang: "",
+          tenHang: "",
+          loai: {
+            id_loai: "",
+            ten_loai: "",
+            products: [] as any
+          }
+        };
+        const resultsData: any = [];
+        const sql = "select * from hang h join loai l on h.id_hang = l.id_hang join san_pham s on l.id_loai = s.id_loai join chi_tiet_sp c on s.id_sp = c.id_sp group by ten_sp;";
+        connection.query(sql, function (err, results) {
+          if (err) throw err;
+          let idHang = results[0].id_hang;
+          let idLoai = results[0].id_loai;
+          results.forEach((item: any) => {
+            if (item.id_hang === idHang && item.id_loai === idLoai) {
+              data.idHang = item.id_hang;
+              data.tenHang = item.ten_hang;
+              data.loai.id_loai = item.id_loai;
+              data.loai.ten_loai = item.ten_loai;
+              data.loai.products.push({
+                idSp: item.id_sp,
+                tenSP: item.ten_sp,
+                giaSp: item.gia,
+                anhSp: item.img,
+              });
+            } else {
+              resultsData.push(data);
+              idHang = item.id_hang;
+              idLoai = item.id_loai;
+              const newData = {
+                idHang: item.id_hang,
+                tenHang: item.ten_hang,
+                loai:{
+                  id_loai: item.id_loai,
+                  ten_loai: item.ten_loai,
+                products: [
+                  {
+                    idSp: item.id_sp,
+                    tenSP: item.ten_sp,
+                    giaSp: item.gia,
+                    anhSp: item.img,
+                  },
+                ]
+              }
+              };
+              data = { ...newData };
+            }
+          });
+          resultsData.push(data);
+          res.json(resultsData);
+        });
+      } catch (error) {
+        res.json({
+          status: 400,
+          body: error,
+        });
+      }
+    }
+  );
+};
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,8 @@
 
 import { serverInit } from 'config/config';
 import { Login } from 'controllers/Auth/Login/Login';
-import { getChiTietSP } from 'controllers/Product/getChiTietTheoSP';
-import { getDSHang } from 'controllers/Product/getDSHang';
-import { getLoai } from 'controllers/Product/getDSLoaiTheoHang';
-import { getDSSP } from 'controllers/Product/getDSSPTheoLoai';
+import { getProduct } from 'controllers/Product/getProduct';
+import { Detail } from 'controllers/Product/getDetail';
 import express from 'express';
 
 const app = express();
@@ -13,8 +11,6 @@ export const routes = () => {
     serverInit(app);
 
     app.use("/auth/signin", Login());
-    app.use("/Product/getDSHang", getDSHang());
-    app.use("/Product/getDSLoaiTheoHang", getLoai());
-    app.use("/Product/getDSSPTheoLoai", getDSSP());
-    app.use("/Product/getChiTietTheoSP", getChiTietSP());
+    app.use("/Product/getProduct", getProduct());
+    app.use("/Product/getDetail", Detail());
 }
