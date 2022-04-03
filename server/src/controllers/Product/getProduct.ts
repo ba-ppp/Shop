77790a9a20907349d71913226f2690ab4f59d70d
khangb1910652 <nguyenduykhang444@.com>
2022-04-03 10:03:45 +0700

    khang

--- server/src/controllers/Product/getProduct.ts
+++ server/src/controllers/Product/getProduct.ts
@@ -11,30 +11,32 @@ export const getProduct = () => {
         let data = {
           idHang: "",
           tenHang: "",
-          loai: {
-            id_loai: "",
-            ten_loai: "",
-            products: [] as any
-          }
+          loai: [] as any,
         };
         const resultsData: any = [];
-        const sql = "select * from hang h join loai l on h.id_hang = l.id_hang join san_pham s on l.id_loai = s.id_loai join chi_tiet_sp c on s.id_sp = c.id_sp group by ten_sp;";
+        const sql =
+          "select * from hang h join loai l on h.id_hang = l.id_hang join san_pham s on l.id_loai = s.id_loai join chi_tiet_sp c on s.id_sp = c.id_sp group by ten_sp;";
         connection.query(sql, function (err, results) {
           if (err) throw err;
           let idHang = results[0].id_hang;
           let idLoai = results[0].id_loai;
+
           results.forEach((item: any) => {
-            if (item.id_hang === idHang && item.id_loai === idLoai) {
+            if (item.id_hang === idHang) {
               data.idHang = item.id_hang;
               data.tenHang = item.ten_hang;
-              data.loai.id_loai = item.id_loai;
-              data.loai.ten_loai = item.ten_loai;
-              data.loai.products.push({
-                idSp: item.id_sp,
-                tenSP: item.ten_sp,
-                giaSp: item.gia,
-                anhSp: item.img,
-              });
+              
+                data.loai.push({
+                  idLoai: item.id_loai,
+                  tenLoai: item.ten_loai,
+                  product: {
+                    idSp: item.id_sp,
+                    tenSP: item.ten_sp,
+                    giaSp: item.gia,
+                    anhSp: item.img,
+                  },
+                });
+               
             } else {
               resultsData.push(data);
               idHang = item.id_hang;
@@ -42,21 +44,19 @@ export const getProduct = () => {
               const newData = {
                 idHang: item.id_hang,
                 tenHang: item.ten_hang,
-                loai:{
+                loai: {
                   id_loai: item.id_loai,
                   ten_loai: item.ten_loai,
-                products: [
-                  {
-                    idSp: item.id_sp,
-                    tenSP: item.ten_sp,
-                    giaSp: item.gia,
-                    anhSp: item.img,
-                  },
-                ]
-              }
+                  products: {
+                      idSp: item.id_sp,
+                      tenSP: item.ten_sp,
+                      giaSp: item.gia,
+                      anhSp: item.img,
+                    }
+                }
               };
               data = { ...newData };
-            }
+            };
           });
           resultsData.push(data);
           res.json(resultsData);
