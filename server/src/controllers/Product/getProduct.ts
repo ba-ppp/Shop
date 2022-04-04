8dcef41ad0d19d76ad72b908fc04baf96b8a9f49
khangb1910652 <nguyenduykhang444@.com>
2022-04-04 07:30:07 +0700

    khang

--- server/src/controllers/Product/getProduct.ts
+++ server/src/controllers/Product/getProduct.ts
@@ -11,11 +11,12 @@ export const getProduct = () => {
         let data = {
           id: "",
           ten: "",
-          loai: {
-            id: "",
-            ten: "",
-            products: [] as any,
-          },
+          loai: [] as any,
+        };
+        let loai = {
+          id: "",
+          ten: "",
+          products: [] as any,
         };
         const resultsData: any = [];
         const sql =
@@ -23,42 +24,65 @@ export const getProduct = () => {
 
         connection.query(sql, function (err, results) {
           if (err) throw err;
-
           results.forEach((item: any) => {
             let idHang = results[0].id_hang;
             let idLoai = results[0].id_loai;
-            if (item.id_hang === idHang) {
-              data.id = item.id_hang;
-              data.ten = item.ten_hang;
-
-              if (item.id_loai !== idLoai) {
-                (data.loai.id = item.id_loai),
-                  (data.loai.ten = item.ten_loai),
-                  data.loai.products.push({
-                    id: item.id_sp,
-                    ten: item.ten_sp,
-                    gia: item.gia,
-                    anh: item.anh,
-                  });
+            if (item.id_loai === idLoai || item.id_loai === loai.id) {
+              loai.id = item.id_loai;
+              loai.ten = item.ten_loai;
+              loai.products.push({
+                id: item.id_sp,
+                ten: item.ten_sp,
+                gia: item.gia,
+                anh: item.anh,
+              });
               
-              }
             } else {
-              resultsData.push(data);
-              idHang = item.id_hang;
+              data.loai.push(loai);
               idLoai = item.id_loai;
-              const newData = {
-                id: item.id_hang,
-                ten: item.ten_hang,
-                loai: {
-                  id: item.id_loai,
-                  ten: item.ten_loai,
-                  products: {
+              const newLoai = {
+                id: item.id_loai,
+                ten: item.ten_loai,
+                products: [
+                  {
                     id: item.id_sp,
                     ten: item.ten_sp,
                     gia: item.gia,
                     anh: item.anh,
                   },
-                },
+                ] as any,
+              };
+
+              loai = { ...newLoai };
+              
+              
+            }
+
+            if (item.id_hang === idHang || item.id_hang === data.id) {
+              data.id = item.id_hang;
+              data.ten = item.ten_hang;
+            } else {
+              resultsData.push(data);
+              // data.loai.push(loai);
+              idHang = item.id_hang;
+              // idLoai = item.id_loai;
+              const newData = {
+                id: item.id_hang,
+                ten: item.ten_hang,
+                loai: [
+                  {
+                    id: item.id_loai,
+                    ten: item.ten_loai,
+                    products: [
+                      {
+                        id: item.id_sp,
+                        ten: item.ten_sp,
+                        gia: item.gia,
+                        anh: item.anh,
+                      },
+                    ],
+                  },
+                ],
               };
               data = { ...newData };
             }
