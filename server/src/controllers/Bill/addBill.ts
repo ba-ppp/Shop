8585334a124c5030bda1d0c40c371079bc924fec
khangb1910652 <nguyenduykhang444@.com>
2022-04-08 17:11:30 +0700

    khang

--- server/src/controllers/Bill/addBill.ts
+++ server/src/controllers/Bill/addBill.ts
@@ -3,12 +3,6 @@ import { connection } from "../../database/mysql";
 
 const router = express.Router();
 
-function uuid() {
-  var temp_url = URL.createObjectURL(new Blob());
-  var uuid = temp_url.toString();
-  URL.revokeObjectURL(temp_url);
-  return uuid.substring(uuid.lastIndexOf('/') + 1); 
-}
 
 export const addBill = () => {
   return router.post(
@@ -21,12 +15,12 @@ export const addBill = () => {
           const sql1 =
           "select * from  san_pham s join mau_sac m on s.id_sp = m.id_sp where s.id_sp = ? and m.mau= ?";
         const sql2 =
-          "call addbill(?,?,?,?,?)";
+          "call addbill(?,?,?,?,?,?)";
         connection.query(sql, [id,rom], function (err, result1) {
           if (err) throw err;
           connection.query(sql1, [id,color], function (err, result2) {
             if (err) throw err;
-            connection.query(sql2, [uuid(),result1[0].id_chitiet,result2[0].id_mau,amount,price*amount,phone,name], function (err, result3) {
+            connection.query(sql2, [result1[0].id_chitiet,result2[0].id_mau,amount,price*amount,phone,name], function (err, result3) {
               if (err) throw err;
               if (result3) {
                 res.json({
@@ -35,7 +29,7 @@ export const addBill = () => {
                 });
               } else {
                 res.json({ status: 500, body: "failed" });
-              }
+              };
           });
         });
       });
