21bc013169a18cba672612730a8d8594ae26cc37
Phuong Tran <phuong.tran@ptnglobalcorp.com>
2022-04-30 15:11:29 +0700

    BE: Fix hash string url

--- server/src/controllers/Bill/momoSNS.ts
+++ server/src/controllers/Bill/momoSNS.ts
@@ -9,30 +9,30 @@ export const momoSNS = () => {
   router.post(
     "/momo",
     async (req: express.Request, res: express.Response) => {
-      console.log("first");
       const { amount } = req.body;
       const amountString = (amount / 1000).toString();
       const requestId = v4();
       const orderId = v4();
-      const stringToHash = `accessKey=VK9in7lVWnI2HcRZ&amount=${amountString}&extraData=&ipnUrl=http://localhost:8080/momo/notification&orderId=${orderId}&orderInfo=Muadt&partnerCode=MOMOVN6L20220429&redirectUrl=http://localhost:3000/payment&requestId=${requestId}&requestType=captureWallet`;
       const payload: any = {
         accessKey: "VK9in7lVWnI2HcRZ",
-        partnerCode: "MOMOVN6L20220429",
-        requestId,
         amount: amountString,
+        extraData: "",
+        ipnUrl: "http://localhost:8080/momo/notification",
         orderId,
         orderInfo: "Muadt",
+        partnerCode: "MOMOVN6L20220429",
         redirectUrl: "http://localhost:3000/payment",
-        ipnUrl: "http://localhost:8080/momo/notification",
+        requestId,
         requestType: "captureWallet",
-        extraData: "",
-        lang: "vi",
       };
+
+      const stringToHash = `accessKey=VK9in7lVWnI2HcRZ&amount=${amountString}&extraData=&ipnUrl=http://localhost:8080/momo/notification&orderId=${orderId}&orderInfo=Muadt&partnerCode=MOMOVN6L20220429&redirectUrl=http://localhost:3000/payment&requestId=${requestId}&requestType=captureWallet`;;
       const myHash = sha256.hmac(
         "mkU3vhIpVoGlvETXuKqJEeFzZFWEZyZu",
         stringToHash
       );
       payload["signature"] = myHash;
+      console.log('payload', payload)
       const response = await axios.post(
         "https://test-payment.momo.vn/v2/gateway/api/create",
         payload
@@ -52,7 +52,6 @@ export const momoSNS = () => {
   router.post(
     "/checkMomo",
     async (req: express.Request, res: express.Response) => {
-      console.log('second')
       const { orderId, requestId } = req.body;
 
       const payload: any = {
@@ -63,7 +62,6 @@ export const momoSNS = () => {
       };
       const payloadURL = new URLSearchParams(payload);
       const payloadString = payloadURL.toString();
-      console.log('payloadString', payloadString)
       const myHash = sha256.hmac(
         "mkU3vhIpVoGlvETXuKqJEeFzZFWEZyZu",
         payloadString
