64440753ba7b632ad3c6f79aa92d92595637dd80
Phuong Tran <phuong.tran@ptnglobalcorp.com>
2022-05-14 14:39:21 +0700

    FE: Fix design home page

--- ui/src/app/slices/toggle.slice.ts
+++ ui/src/app/slices/toggle.slice.ts
@@ -4,14 +4,19 @@ const ToggleSlice = createSlice({
   name: "toggle",
   initialState: {
     isOpenSlideBar: true as boolean,
+    hasMenuSelect: false as boolean
   },
   reducers: {
     toggleSlideBar: (state, action: PayloadAction<boolean>) => {
       state.isOpenSlideBar = action.payload;
     },
+    toggleMenuSelect: (state, action: PayloadAction<boolean>) => {
+      state.hasMenuSelect = action.payload;
+    }
+
   },
 });
 
-export const { toggleSlideBar } = ToggleSlice.actions;
+export const { toggleSlideBar, toggleMenuSelect } = ToggleSlice.actions;
 
 export const ToggleReducers = ToggleSlice.reducer;
---
+++ ui/src/components/Search/Search.tsx
@@ -0,0 +1,27 @@
+/** @jsxImportSource @emotion/react */
+import "twin.macro";
+import React from "react";
+import { ReactComponent as SearchBackground } from "asset/images/search-background.svg";
+
+export const Search = () => {
+  return (
+    <div tw="w-full height[70vh]">
+      <div tw="w-1/3 h-full mx-auto mt-20 text-center border-2 rounded-2xl border-solid shadow-2xl">
+        <SearchBackground tw="w-full h-1/2 mx-auto mt-12" />
+        <div tw="text-2xl mt-12">Tra cứu thông tin mua hàng</div>
+        <input
+          style={{
+            backgroundImage:
+              "url('https://cdn.tgdd.vn/mwgcart/orderhistory/images/icon-phone-blue.png')",
+          }}
+          tw="pl-14 background-size[14px 21px] background-position[25px 50%] bg-no-repeat border-2 rounded-3xl width[300px] height[50px] mt-6 mb-5 focus:outline-none"
+          type="text"
+          placeholder="Nhập số điện thoại đặt hàng"
+        />
+        <button tw="bg-style-purple-2 hover:bg-style-purple-3 text-white font-bold py-3 px-4 rounded items-center space-x-2 block mx-auto">
+          Tìm kiếm
+        </button>
+      </div>
+    </div>
+  );
+};
